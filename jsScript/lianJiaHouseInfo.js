c2be582a557a8631bba4c26fd56122ff8c2f4c54
panFeng <595464742@qq.com>
2025-04-17 10:33:18 +0800

    优化js 追加house

--- jsScript/lianJiaHouseInfo.js
+++ jsScript/lianJiaHouseInfo.js
@@ -1,91 +1,144 @@
+// 获取当前 URL
+const currentUrl = window.location.href;
+// 判断当前 URL 是否以指定字符串开头
+const isMatch = currentUrl.startsWith('https://clogin.lianjia.com/');
+
+
+if (isMatch) {
+    console.log('当前 是登录页面');
+} else {
+    console.log('当前 URL 不以 "https://clogin.lianjia.com/" 开头');
+
 // 获取所有的 <li> 节点
-const listItems = document.querySelectorAll('.listContent > li');
-const houseDatas = []
+    const listItems = document.querySelectorAll('.listContent > li');
+    const houseDatas = []
 // 遍历每个 <li> 节点
-listItems.forEach((listItem) => {
-    // 获取楼盘名字
-    const titleElement = listItem.querySelector('.title a');
-    const estateName = titleElement ? titleElement.textContent.split(' ')[0] : null;
+    listItems.forEach((listItem) => {
 
-    // 获取面积
-    const areaElement = listItem.querySelector('.title a');
-    const areaText = areaElement ? areaElement.textContent.match(/(\d+\.\d+)平米/) : null;
-    const area = areaText ? parseFloat(areaText[1]) : null;
+        // 获取楼盘名字
+        const titleElement = listItem.querySelector('.title a');
+        const estateName = titleElement ? titleElement.textContent.split(' ')[0] : null;
+        if (arePlaceNamesSimilar(houseName, estateName)) {
+            // 获取面积
+            const areaElement = listItem.querySelector('.title a');
+            const areaText = areaElement ? areaElement.textContent.match(/(\d+\.\d+)平米/) : null;
+            const area = areaText ? parseFloat(areaText[1]) : null;
 
-    // 获取交易时间
-    const dealDateElement = listItem.querySelector('.dealDate');
-    const dealDate = dealDateElement ? dealDateElement.textContent : null;
+            // 获取交易时间
+            const dealDateElement = listItem.querySelector('.dealDate');
+            const dealDate = dealDateElement ? dealDateElement.textContent : null;
 
-    // 获取交易价格
-    const totalPriceElement = listItem.querySelector('.totalPrice .number');
-    const totalPrice = totalPriceElement ? parseFloat(totalPriceElement.textContent) : null;
+            // 获取交易价格
+            const totalPriceElement = listItem.querySelector('.totalPrice .number');
+            const totalPrice = totalPriceElement ? parseFloat(totalPriceElement.textContent) : null;
 
-    // 获取是否精装
-    const houseInfoElement = listItem.querySelector('.houseInfo');
-    const isRefurbished = houseInfoElement && houseInfoElement.textContent.includes('精装');
+            // 获取是否精装
+            const houseInfoElement = listItem.querySelector('.houseInfo');
+            const isRefurbished = houseInfoElement && houseInfoElement.textContent.includes('精装');
 
-    // 获取挂牌价
-    const dealCycleTxtElement = listItem.querySelector('.dealCycleTxt');
-    const listingPriceText = dealCycleTxtElement ? dealCycleTxtElement.textContent.match(/挂牌(\d+)万/) : null;
-    const listingPrice = listingPriceText ? parseFloat(listingPriceText[1]) : null;
+            // 获取挂牌价
+            const dealCycleTxtElement = listItem.querySelector('.dealCycleTxt');
+            const listingPriceText = dealCycleTxtElement ? dealCycleTxtElement.textContent.match(/挂牌(\d+)万/) : null;
+            const listingPrice = listingPriceText ? parseFloat(listingPriceText[1]) : null;
 
-    houseDatas.push({
-        "楼盘名字": estateName,
-        "面积": area,
-        "交易时间": dealDate,
-        "交易价格": totalPrice,
-        "是否精装": isRefurbished,
-        "挂牌价": listingPrice,
-    })
-
-});
-
-
-function postData() {
-    /*const data = {
-        "address": "abc", "info": {
-            "楼盘名字": "示例楼盘",
-            "面积": "100平",
-            "交易时间": "2025-04-14",
-            "交易价格": "100万",
-            "是否精装": "是",
-            "挂牌价": "120万"
+            houseDatas.push({
+                "楼盘名字": estateName,
+                "面积": area,
+                "交易时间": dealDate,
+                "交易价格": totalPrice,
+                "是否精装": isRefurbished,
+                "挂牌价": listingPrice,
+                //"区域": houseArea,
+            })
         }
-    };*/
+    });
 
-    //houseName是python注入的
-    const data = {
-        "address": houseName, "info": houseDatas
-    };
-    if (isInit) {
-        const pagesButton = document.querySelectorAll('.page-box.house-lst-page-box > a');
-        let pages = pagesButton.length > 4 ? 4 : pagesButton.length
-        console.log(pageNum, pages, pagesButton.length)
-        if (pageNum <= 4 && pages > pageNum) {
-            data.nextPage = pageNum + 1;
-        }
+    function postData() {
+        /*const data = {
+            "address": "abc", "info": {
+                "楼盘名字": "示例楼盘",
+                "面积": "100平",
+                "交易时间": "2025-04-14",
+                "交易价格": "100万",
+                "是否精装": "是",
+                "挂牌价": "120万"
+            }
+        };*/
 
-    }
-    console.log("data:", data)
+        //houseName是python注入的
+        const data = {
+            "address": houseName, "info": houseDatas
+        };
 
-
-    const xhr = new XMLHttpRequest();
-    xhr.open('POST', 'http://127.0.0.1:5000/add_data', true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4) {
-            if (xhr.status === 200) {
-                console.log('数据发送成功：', xhr.responseText);
-            } else {
-                console.error('数据发送失败，状态码：', xhr.status);
+        if (isInit && houseDatas.length > 0) {
+            let lastHouse = houseDatas[houseDatas.length - 1]
+            if (isGreaterThanYear(lastHouse["交易时间"])) {
+                const pagesButton = document.querySelectorAll('.page-box.house-lst-page-box > a');
+                let pages = pagesButton.length > 4 ? 4 : pagesButton.length
+                console.log(pageNum, pages, pagesButton.length)
+                if (pageNum <= 4 && pages > pageNum) {
+                    data.nextPage = pageNum + 1;
+                }
             }
         }
-    };
+        console.log("data:", data)
 
-    xhr.send(JSON.stringify(data));
+
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', 'http://127.0.0.1:5000/add_data', true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    console.log('数据发送成功：', xhr.responseText);
+                } else {
+                    console.error('数据发送失败，状态码：', xhr.status);
+                }
+            }
+        };
+
+        xhr.send(JSON.stringify(data));
+    }
+
+    postData();
 }
 
-postData();
+//判读啊时间大于2024
+function isGreaterThanYear(dateString, year = 2024) {
+    // 将输入的日期字符串转化为 Date 对象
+    const date = new Date(dateString);
+
+    // 创建一个指定年份的日期对象，这里用的是指定年份的1月1日
+    const compareDate = new Date(year, 0, 1); // 比如 2024-01-01
+
+    // 比较两个日期对象的时间戳
+    return date > compareDate;
+}
+
+//楼盘名字相似度判断1
+function getJaccardSimilarity(str1, str2) {
+    // 将字符串转化为集合，去除重复的字符
+    const set1 = new Set(str1.split(''));
+    const set2 = new Set(str2.split(''));
+
+    // 计算交集
+    const intersection = new Set([...set1].filter(x => set2.has(x)));
+
+    // 计算并集
+    const union = new Set([...set1, ...set2]);
+
+    // 返回Jaccard相似度
+    return intersection.size / union.size;
+}
+
+//楼盘名字相似度判断2
+function arePlaceNamesSimilar(name1, name2, threshold = 0.6) {
+    // 使用Jaccard相似度计算地名相似度
+    const similarity = getJaccardSimilarity(name1, name2);
+
+    // 如果相似度高于阈值，则认为是相似的地名
+    return similarity >= threshold;
+}
 
 //nextUrl()
--- src/spider.py
+++ src/spider.py
@@ -14,7 +14,144 @@ print(os.path.join(current_dir, '..', 'jsScript', 'lianJiaHouseInfo.js'))
 #     "蜀南春郡": "https://cd.lianjia.com/chengjiao/rs%E8%9C%80%E5%8D%97%E6%98%A5%E9%83%A1/",
 #     "凯莱丽景": "https://cd.lianjia.com/chengjiao/rs%E5%87%AF%E8%8E%B1%E4%B8%BD%E6%99%AF/",
 # }
-initHouseList = ["蜀南春郡", "凯莱丽景", "红树湾", "光明城市","鑫苑鑫都汇","戛纳湾金棕榈","远大都市风景","慕和南道","融创南湖逸家","保利创智锦城"]
+initHouseList = [
+    "蜀南春郡",
+    "凯莱丽景",
+    "红树湾",
+    "光明城市",
+    "鑫苑鑫都汇",
+    "戛纳湾金棕榈",
+    "远大都市风景",
+    "慕和南道",
+    "融创南湖逸家",
+    "保利创智锦城",
+    "川发天府上城",
+    "远大中央公园",
+    "锦官丽城亲水湾",
+    "蜀郡",
+    "中德英伦联邦",
+    "华润凤凰城", "保利百合花园",
+    "阳光华苑",
+    "保利叶语",
+    "保利心语花园",
+    "中海锦江城",
+    "中海兰庭",
+    "朗基天香",
+    "时代晶科名苑",
+    "南城都汇汇朗园五期",
+#上面是我提供的，
+#下面的是AI提供的
+
+    #锦江区
+    "伊泰天骄",
+    "人居东湖长岛",
+    "建发央玺",
+    "滨河湾",
+    "环球汇天誉",
+    "中化润达丰滨江樾城",
+    "塔子山壹号",
+    "万科城市花园",
+    "锦天府",
+    "人居锦城峰荟荟澜阁",
+    "华都美林湾",
+    "锦江国际花园",
+    "青羊区",
+    "绿地成都新里城",
+    "成都花园别墅",
+    "金林半岛",
+    "金沙鹭岛四期",
+    "博瑞都市花园",
+    "中大君悦金沙五期",
+    "远洋万和公馆",
+    "蓝光雍锦园别墅",
+    "金沙云庭",
+    "华润金悦湾",
+    "仁和金沙",
+    "凯德风尚",
+    "石人小区",
+    "优品道曦岸",
+
+    #武侯区
+    "鹭岛国际四期",
+    "丽景华庭二期",
+    "中华名园",
+    "置信丽都花园",
+    "双楠小区",
+    "龙湖金楠天街",
+    "保利花园",
+    "中粮祥云国际生活区",
+    "蓝光雍锦世家",
+    "首信汇",
+    "红牌楼广场小区",
+    "新希望花园",
+    "棕北小区",
+    "棕南小区",
+    "锦官新城",
+
+    #金牛区
+    "中大君悦金沙五期",
+    "西城首峻",
+    "华侨城纯水岸",
+    "蓝光花满庭",
+    "保利公园里",
+    "天回镇银杏园小区",
+    "凯德风尚",
+    "万科金域西岭",
+    "量力健康城",
+    "营门口小区",
+    "蜀汉路小区",
+    "星河路小区",
+    "五块石玉局庵东路小区",
+
+    #成华区
+    "保利康桥",
+    "河畔华苑二期",
+    "华润二十四城",
+    "蓝光时代",
+    "中铁建青秀城",
+    "首创天禧",
+    "融创玖阙府",
+    "万科魅力之城",
+    "隆鑫九熙",
+    "蓝光东方天地",
+    "成华奥园广场",
+
+
+    #高新区
+    "中大文儒德",
+    "中海城南一号",
+    "复地金融岛",
+    "中海九号公馆",
+    "誉峰二期",
+    "伊泰天骄",
+    "中洲锦城湖岸",
+    "建发翡翠鹭洲",
+    "朗基御今缘",
+    "凯德世纪名邸",
+    "大源欢乐颂",
+    "保利心语花园",
+    "华润凤凰城",
+    "中海兰庭",
+    "南苑小区",
+
+    #天府新区
+    "麓湖生态城麒麟荟",
+    "麓湖生态城云树",
+    "蔚蓝卡地亚",
+    "中海右岸",
+    "远大中央公园",
+    "南湖国际社区",
+    "万科翡翠公园",
+    "保利天空之城",
+    "合能铂悦华庭",
+    "红星美凯龙生活广场",
+    "海伦春天",
+    "雅居乐花园",
+    "融创玖棠府",
+    "天投北鑫苑",
+]
+initHouseList = list(set(initHouseList))
+
 addHouseList = ["蜀南春郡", "凯莱丽景", "红树湾", "光明城市"]
 runType = "init"  # add | init
 houseList = []
@@ -73,7 +210,8 @@ def initSpider():
     # 使用profile，获取已有的登录信息
     if system == 'Darwin':
         print("当前系统是 macOS")
-        options.add_argument(r'--user-data-dir=/Users/panfeng/Library/Application Support/Google/Chrome')
+        # options.add_argument(r'--user-data-dir=/Users/panfeng/Library/Application Support/Google/Chrome')
+        options.add_argument(r'--user-data-dir=/Users/panfeng/coder/myProject/houseSpider/chromeInfo')
     elif system == 'Windows':
         print("当前系统是 Windows")
         # options.add_argument(r"--user-data-dir=C:\Users\59546\AppData\Local\Google\Chrome\User Data")
@@ -85,6 +223,7 @@ def initSpider():
     options.add_argument("--profile-directory=spiderProfile")
     # options.add_argument("--profile-directory=Default")
     options.headless = False
+    options.add_argument("--disable-extensions")
     # options.headless = True
     options.add_argument("--disable-extensions")
     options.add_argument("--no-sandbox")
